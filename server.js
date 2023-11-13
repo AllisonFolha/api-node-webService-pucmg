eb9c5d707b662e094d6eed95773d8668707b4b59
Allison Folha <allison.folha@consultores.rnp.br>
2023-11-13 19:52:20 -0300

    chore: configurando o projeto

--- server.js
+++ server.js
@@ -1,91 +1,66 @@
-const express = require("express");
-const apicache = require("apicache");
+const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-let cache = apicache.middleware;
-
-// Dados
-const pessoas = [
-  { id: 1, nome: "José" },
-  { id: 2, nome: "Roberto" },
-  { id: 3, nome: "Maria" },
-];
-
-const carros = [
-  { id: 1, modelo: "Hb20" },
-  { id: 2, modelo: "Gol" },
-  { id: 3, modelo: "Palio" },
-];
-
-const animais = [
-  { id: 1, nome: "Gato" },
-  { id: 2, nome: "Sapo" },
-  { id: 3, nome: "Papagaio" },
-];
-
-// Status API
-app.get("/", (req, res) => {
-  const status = { status: 200, message: "API On" };
-  res.json(status);
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     summary: Retorna uma mensagem de boas-vindas
+ *     responses:
+ *       '200':
+ *         description: OK
+ */
+app.get('/', (req, res) => {
+  res.send('Bem-vindo à sua API Node.js!');
 });
 
-// Rotas
-const router = express.Router();
-
-router.get("/pessoas", (req, res) => {
-  res.json(pessoas);
+/**
+ * @openapi
+ * /users:
+ *   get:
+ *     summary: Retorna uma lista de usuários
+ *     responses:
+ *       '200':
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             example:
+ *               - id: 1
+ *                 name: 'Usuário 1'
+ *               - id: 2
+ *                 name: 'Usuário 2'
+ */
+app.get('/users', (req, res) => {
+  const users = [
+    { id: 1, name: 'Usuário 1' },
+    { id: 2, name: 'Usuário 2' },
+  ];
+  res.json(users);
 });
 
-router.get("/pessoas/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const pessoa = pessoas.find((p) => p.id === id);
-  if (pessoa) {
-    res.json(pessoa);
-  } else {
-    res.json({});
-  }
-});
+// Configuração do Swagger
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Sua API Node.js',
+      version: '1.0.0',
+      description: 'Descrição da sua API',
+    },
+  },
+  apis: ['server.js'], 
+};
 
-router.get("/carros", (req, res) => {
-  res.json(carros);
-});
+const openapiSpecification = swaggerJsdoc(options);
 
-router.get("/carros/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const carro = carros.find((c) => c.id === id);
-  if (carro) {
-    res.json(carro);
-  } else {
-    res.json({});
-  }
-});
+// Middleware para servir a documentação Swagger
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(openapiSpecification));
 
-router.get("/animais", (req, res) => {
-  res.json(animais);
-});
-
-router.get("/animais/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const animal = animais.find((a) => a.id === id);
-  if (animal) {
-    res.json(animal);
-  } else {
-    res.json({});
-  }
-});
-
-app.use("/api", cache("5 minutes"), router);
-
-// Ouvir Server na Porta 3000
-app.listen(PORT, () => {
-  console.log("Server Started");
-  console.log(`http://localhost:${PORT}`);
-  console.log("\nRoutes:\n");
-  console.log("/api/pessoas");
-  console.log("/api/pessoas/:id");
-  console.log("/api/carros");
-  console.log("/api/carros/:id");
-  console.log("/api/animais");
-  console.log("/api/animais/:id");
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
